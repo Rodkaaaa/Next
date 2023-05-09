0c311c11f866e2624e9d1ac7babcdddf388db883
rodkaaaa <frederickcid@gmail.com>
2023-05-09 17:02:33 -0400

    adadad

--- 06-open-jira/src/pages/_app.tsx
+++ 06-open-jira/src/pages/_app.tsx
@@ -1,22 +1,24 @@
-import { EntriesProvider } from "@/context/entries";
-import { UIProvider } from "@/context/ui";
-import "@/styles/globals.css";
-import { darkTheme, lightTheme } from "@/themes";
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline } from "@mui/material";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import { UIProvider } from '../context/ui';
+import { EntriesProvider } from '../context/entries';
+
+import { lightTheme, darkTheme } from '../themes';
+
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <EntriesProvider>
       <UIProvider>
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme={ darkTheme }>
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
       </UIProvider>
     </EntriesProvider>
-  );
+  )
 }
 
 export default App;
