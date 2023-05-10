5f19f47ddee17d173a0c01dffa612f280cf3b61c
rodkaaaa <frederickcid@gmail.com>
2023-05-10 08:23:29 -0400

    Terminando OpenJira

--- 06-open-jira/src/context/entries/EntriesProvider.tsx
+++ 06-open-jira/src/context/entries/EntriesProvider.tsx
@@ -1,46 +1,69 @@
 import { useReducer } from "react";
-import { EntriesContext } from "./";
-import { EntriesReducer } from "./";
-import { Entry } from "@/interfaces";
 import { v4 as uuidv4 } from "uuid";
+
+import { Entry } from "../../interfaces";
+
+import { EntriesContext, EntriesReducer } from "./";
 
 export interface EntriesState {
   entries: Entry[];
 }
-const ENTRIES_INITIAL_STATE: EntriesState = {
+
+const Entries_INITIAL_STATE: EntriesState = {
   entries: [
     {
       _id: uuidv4(),
       description:
-        "Pendiente: Commodo exercitation officia est consectetur deserunt anim fugiat nulla anim nisi cupidatat.",
-      createAt: Date.now(),
+        "Pendiente: Proident dolor duis elit sunt qui dolor laborum veniam ea laboris qui consequat.",
       status: "pending",
+      createAt: Date.now(),
     },
     {
       _id: uuidv4(),
       description:
-        "En progreso: Commodo exercitation officia est consectetur deserunt anim fugiat nulla anim nisi cupidatat.",
-      createAt: Date.now() - 100000,
+        "En-Progreso Veniam in cupidatat adipisicing Lorem sunt est est ex cillum laboris fugiat officia fugiat.",
       status: "in-progress",
+      createAt: Date.now() - 1000000,
     },
     {
       _id: uuidv4(),
       description:
-        "Finalizado: Commodo exercitation officia est consectetur deserunt anim fugiat nulla anim nisi cupidatat.",
-      createAt: Date.now() - 10000,
+        "Terminadas: Commodo veniam aliqua tempor officia officia non laborum.",
       status: "finished",
+      createAt: Date.now() - 100000,
     },
   ],
 };
 type Props = {
   children: JSX.Element;
 };
+
 export const EntriesProvider = ({ children }: Props) => {
-  const [state, dispatch] = useReducer(EntriesReducer, ENTRIES_INITIAL_STATE);
+  const [state, dispatch] = useReducer(EntriesReducer, Entries_INITIAL_STATE);
+
+  const addNewEntry = (description: string) => {
+    const newEntry: Entry = {
+      _id: uuidv4(),
+      description,
+      createAt: Date.now(),
+      status: "pending",
+    };
+
+    dispatch({ type: "[Entry] Add-Entry", payload: newEntry });
+  };
+
+  const updateEntry = (entry: Entry) => {
+    dispatch({ type: "[Entry] Entry-Updated", payload: entry });
+  };
+
   return (
     <EntriesContext.Provider
       value={{
         ...state,
+
+        // Methods
+        addNewEntry,
+        updateEntry,
       }}
     >
       {children}
