30d32f01bbb0ad0337edaecd077151279398c7ff
rodkaaa <frederickcid@gmail.com>
2023-06-08 14:10:23 -0400

    cambios en 07

--- 07-cookies-master/src/pages/_app.tsx
+++ 07-cookies-master/src/pages/_app.tsx
@@ -1,14 +1,31 @@
-import '@/styles/globals.css'
-import { darkTheme } from '@/themes'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { darkTheme } from "@/themes";
+import type { AppContext, AppProps } from "next/app";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { NextPageContext } from "next";
+import Cookies from 'js-cookie';
+import { customTheme } from '../themes/custom-theme';
 
+export default function App({ Component, pageProps, theme }: AppProps) {
+  console.log({ theme });
 
-export default function App({ Component, pageProps, }: AppProps) {
   return (
-    <ThemeProvider theme={ darkTheme }>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
+
+App.getInitialProps = async (appContext: AppContext) => {
+  
+  
+  const cookies = appContext.ctx.req ? (appContext.ctx.req as any).cookies : {theme: 'light'}
+  
+  console.log(cookies)
+  
+  return {
+
+    theme: cookies 
+  };
+};
--- 07-cookies-master/src/pages/theme-changer.tsx
+++ 07-cookies-master/src/pages/theme-changer.tsx
@@ -1,36 +1,86 @@
-import React, { ChangeEvent, useState } from 'react'
-import { Layout } from '@/components/layouts'
-import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
+import React, { ChangeEvent, FC, useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
+import { Layout } from "@/components/layouts";
+import Cookies from "js-cookie";
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import axios from "axios";
 
-const ThemeChanger = () => {
+interface Props {
+  theme: string;
+  name: string;
+}
 
-  const [currentTheme, setCurrentTheme] = useState('light')
+const ThemeChanger = ({ theme }: Props) => {
+  const [currentTheme, setCurrentTheme] = useState(theme);
 
-  const onThemeChange = (event: ChangeEvent<HTMLInputElement>) =>{
-    const selectedTheme= (event.target.value);
-    setCurrentTheme(selectedTheme)
-    console.log(selectedTheme)
-  } 
+  const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedTheme = event.target.value;
+    setCurrentTheme(selectedTheme);
+    localStorage.setItem("theme", selectedTheme);
+    Cookies.set("themeCookies", selectedTheme);
+  };
+
+  const onClick = async () => {
+    const { data } = await axios.get("/api/hello");
+
+    //console.log({ data });
+  };
+
+  useEffect(() => {
+    //console.log("LocalStorage: ", localStorage.getItem("theme"));
+    //console.log("Cookies: ", localStorage.getItem("theme"));
+  }, []);
 
   return (
     <Layout>
-        <Card>
-          <CardContent>
-            <FormControl>
-              <FormLabel>Tema</FormLabel>
-              <RadioGroup
-                value={currentTheme}
-                onChange={onThemeChange}
-              >
-                <FormControlLabel value='light' control={<Radio />} label='Light'/>
-                <FormControlLabel value='dark' control={<Radio />} label='Dark'/>
-                <FormControlLabel value='custom' control={<Radio />} label='Custom'/> 
-              </RadioGroup>
-            </FormControl>
-          </CardContent>
-        </Card>
-    </Layout>
-  )
-}
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Tema</FormLabel>
+            <RadioGroup  value={currentTheme} onChange={onThemeChange}>
+              <FormControlLabel
+                value="light"
+                control={<Radio />}
+                label="Light"
+              />
+              <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+              <FormControlLabel
+                value="custom"
+                control={<Radio />}
+                label="Custom"
+              />
+            </RadioGroup>
+          </FormControl>
 
-export default ThemeChanger
+          <Button onClick={onClick}>Solicitud</Button>
+        </CardContent>
+      </Card>
+    </Layout>
+  );
+};
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { theme = "light", name = "No Name" } = req.cookies;
+
+  const validsTheme = ["light", "dark", "custom"];
+
+  return {
+    props: {
+      theme: validsTheme.includes(theme) ? theme : "dark",
+      name,
+    },
+  };
+};
+
+export default ThemeChanger;
